a6fd867a8d3324bf9937d1f25e0f65fa00c84585
Accel <58154200+Accel-learn-how-to-code@users.noreply.github.com>
2022-03-29 17:52:18 +0700

    connect redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
   imports: [
@@ -12,6 +13,14 @@ import { AuthModule } from './auth/auth.module';
     CatsModule,
     MongooseModule.forRoot('mongodb://localhost/nest'),
     AuthModule,
+    RedisModule.forRoot({
+      config: {
+        url: 'redis://redis-10671.c11.us-east-1-3.ec2.cloud.redislabs.com:10671',
+        password: 'gCI74ZOIYxl6NhTjWWPW3hNY41WlHy0V',
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
